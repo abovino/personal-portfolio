be858eb5d2c2e4909f085a4e954629c57798400f
Angelo Bovino <angelo.bovino@outlook.com>
2016-12-01 22:28:51 -0500

    update

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var path = require('path');
 
 var app = express();
 
-/*app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));*/
+/*app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/views')));*/
 
-app.use(express.static('public'));
-app.use(express.static('views'));
+app.set('views', './views');
+
+/*app.use(express.static('public'));
+app.use(express.static('views'));*/
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+  res.render('index.html');
 });
 
 app.listen(3000, function() {
